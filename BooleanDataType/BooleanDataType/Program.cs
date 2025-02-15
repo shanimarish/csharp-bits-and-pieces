98e677ed53716b3ce4a4b681b081d095d5ebf2a6
Shani <74494158+shanimarish@users.noreply.github.com>
2025-02-16 02:44:38 +0800

    Create BooleanDataType

---
+++ BooleanDataType/BooleanDataType/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool value = true;
+            Console.WriteLine(value);
+
+            bool isFemale = true;
+            Console.WriteLine(isFemale);
+
+            Console.ReadLine();
+        }
+    }
+}
