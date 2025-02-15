c44da168f81529fbf3870030da754c3b87192659
Shani <74494158+shanimarish@users.noreply.github.com>
2025-02-16 03:30:56 +0800

    Update Remainder

--- Remainder/Remainder/Program.cs
+++ Remainder/Remainder/Program.cs
@@ -13,8 +13,11 @@ namespace Remainder
             int firstNum = 10;
             int secondNum = 3;
 
-            Console.WriteLine(firstNum % secondNum); //% will give us the remainder.
-                                                     //% is also helpful to know if the number is even or odd.
+            Console.WriteLine(firstNum % secondNum);
+            //% will give us the remainder.                            
+            //% is also helpful to know if the number is even or odd.
+
+            Console.ReadLine();
         }
     }
 }
