404d17d3e1ed34c8e8da97b436d25c57b07fdf5d
Shani <74494158+shanimarish@users.noreply.github.com>
2025-02-16 03:05:41 +0800

    Create Remainder

---
+++ Remainder/Remainder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = 10;
+            int secondNum = 3;
+
+            Console.WriteLine(firstNum % secondNum); //% will give us the remainder.
+                                                     //% is also helpful to know if the number is even or odd.
+        }
+    }
+}
