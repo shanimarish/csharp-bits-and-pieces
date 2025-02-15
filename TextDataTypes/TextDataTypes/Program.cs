24aa05de2997f27e9aeb0270b94974b80106268d
Shani <74494158+shanimarish@users.noreply.github.com>
2025-02-15 22:34:32 +0800

    Create TextDataTypes

---
+++ TextDataTypes/TextDataTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Abba"; //Double quotation for string (Speech Marks).
+            char letter = 'a'; //Single quotation for char (Apostrophes).
+
+            //String can be empty while char can't but we can use '\0'.
+
+            Console.Write("Your name is: "); //Console.Write will not add another line to the output.
+            Console.Write(name);
+
+            Console.WriteLine();
+            Console.Write(letter);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TextDataTypes/TextDataTypes/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TextDataTypes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TextDataTypes")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d59eb23c-eca6-4f08-a7c7-ca650d00a947")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
