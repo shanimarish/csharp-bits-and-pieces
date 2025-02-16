1b441b0ade11d4f095bc0ca8adb647f126574f4e
Shani <74494158+shanimarish@users.noreply.github.com>
2025-02-16 20:35:40 +0800

    Create NumericFormatting

---
+++ NumericFormatting/NumericFormatting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = 1000D / 12.34D;
+
+            Console.WriteLine(value);
+            Console.WriteLine(string.Format("{0:0.00}", value));
+
+            Console.ReadLine();
+        }
+    }
+}
