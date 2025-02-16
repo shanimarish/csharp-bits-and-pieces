e1b50f3982c9f3bcbc6a2569a2e46a019199b153
Shani <74494158+shanimarish@users.noreply.github.com>
2025-02-16 14:07:51 +0800

    Create ForLoops

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a message you want to repeat: ");
+            string message = Console.ReadLine();
+            Console.Write("How many times do you want to repeat your message?: ");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+            if (loopCounter <= 0)
+            {
+                Console.WriteLine("Please enter a value above zero.");
+            }
+            else
+            {
+                for (int i = 0; i < loopCounter; i++)
+                {
+                    Console.WriteLine(message);
+                }
+            }
+
+            Console.ReadLine();  
+        }
+    }
+}
