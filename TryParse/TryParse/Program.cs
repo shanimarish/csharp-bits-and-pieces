d07849cd2556ed3bd79312c8967e15d1eca37eab
Shani <74494158+shanimarish@users.noreply.github.com>
2025-02-16 20:51:53 +0800

    Create TryParse

---
+++ TryParse/TryParse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string numInput = Console.ReadLine();
+            int number = 0;
+
+            bool success = int.TryParse(numInput, out number);
+
+            if (success)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert!");
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
